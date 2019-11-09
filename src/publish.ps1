62f39ddc9a752d360129af05bc2384cf933fbd2c
Christian Hermann <c.hermann@bitbeans.de>
2019-11-09 17:45:04 +0100

    Added publish script

---
+++ src/publish.ps1
@@ -0,0 +1,4 @@
+Remove-Item "F:\x64\*" -recurse
+Remove-Item "F:\x86\*" -recurse
+dotnet publish -c release -r win-x64 /p:Platform=x64 /p:PublishSingleFile=true /p:DebugType=None -o "F:\x64"
+dotnet publish -c release -r win-x86 /p:Platform=x86 /p:PublishSingleFile=true /p:DebugType=None -o "F:\x86"
