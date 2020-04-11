769d6f660c82e7076cbbd55de52aed9697f622ba
Christian Hermann <c.hermann@bitbeans.de>
2020-04-11 12:05:57 +0200

    Update publish.ps1

--- src/publish.ps1
+++ src/publish.ps1
@@ -1,4 +1,4 @@
-Remove-Item "F:\x64\*" -recurse
-Remove-Item "F:\x86\*" -recurse
-dotnet publish -c release -r win-x64 /p:Platform=x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None -o "F:\x64"
-dotnet publish -c release -r win-x86 /p:Platform=x86 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None -o "F:\x86"
+Remove-Item "E:\x64\*" -recurse
+Remove-Item "E:\x86\*" -recurse
+dotnet publish -c release -r win-x64 /p:Platform=x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None -o "E:\x64"
+dotnet publish -c release -r win-x86 /p:Platform=x86 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None -o "E:\x86"
